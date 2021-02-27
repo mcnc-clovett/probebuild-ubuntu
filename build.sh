b7a075858d0697bd662118457e4dba01ce9b53de
Chris Lovett <clovett@mcnc.org>
2021-02-26 21:32:11 -0500

    Tried to use yum like an idiot

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ curl -fsSL https://get.docker.com/ | sh
 sudo usermod -aG docker `whoami`
 
 # Install other supporting applications
-yum install -y tcpdump tshark iperf iperf3 apache2 docker-compose mosh
+sudo apt install -y tcpdump tshark iperf iperf3 apache2 docker-compose mosh
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always \
 	-v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
