2242c64325cd64f6c3067e2ad78bff48526799eb
Chris Lovett <clovett@mcnc.org>
2021-04-11 15:53:28 -0400

    Added cockpit and adjusted ntop defaults

--- build.sh
+++ build.sh
@@ -8,14 +8,16 @@ sudo update-ca-certificates
 sudo sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
 
 # Install docker using official install script
-curl -fsSL https://get.docker.com/ | sh
-sudo usermod -aG docker `whoami`
+if [ -x /usr/bin/docker ]; then
+  curl -fsSL https://get.docker.com/ | sh
+  sudo usermod -aG docker `whoami`
+fi
 
 # Install other supporting applications
-sudo apt install -y tcpdump tshark iperf iperf3 apache2 docker-compose mosh
+sudo apt install -y cockpit tcpdump tshark iperf iperf3 apache2 docker-compose mosh
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always \
-	-v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+	-v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
 # Allow access to probe tools through the local firewall
 sudo sed -i 's/IPV6=yes/IPV6=no/' /etc/default/ufw
@@ -26,6 +28,7 @@ sudo ufw allow 3000/tcp
 sudo ufw allow 3001/tcp
 sudo ufw allow 5001
 sudo ufw allow 5201
+sudo ufw allow 9090/tcp
 sudo ufw enable
 
 # Add iperf service files and enable/start all services
@@ -40,9 +43,13 @@ sudo systemctl enable iperf3
 # Create homepage for web-based tools
 sudo cp ./index.html /var/www/html/
 
-echo "You now need to logout and log back in. Then run 'docker-compose up -d' from this directory."
+echo "
 
-# Download and start docker containers
-#docker-compose up -d
-#docker cp createdevices.sh cacti:/
-#docker exec cacti chmod +x /createdevices.sh
+*######################################################*
+#                                                      #
+# You now need to logout and log back in.              #
+#                                                      #
+# Then run 'docker-compose up -d' from this directory. #
+#                                                      #
+*######################################################*
+"
